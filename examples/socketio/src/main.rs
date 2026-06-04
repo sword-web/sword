19536ab7c12dbc766a20fcb7cdf7c99172c50f12
MrRevillod <lrevillod2022@alu.uct.cl>
2026-06-04 11:26:34 -0400

    refactor: remove 'async_trait' from public reexport and update docs

--- examples/socketio/src/main.rs
+++ examples/socketio/src/main.rs
@@ -3,18 +3,14 @@ pub mod database;
 
 use crate::{chat::ChatModule, database::Database};
 use sword::prelude::*;
-use sword_layers::cors::{CorsConfig, CorsLayer};
 
 #[sword::main]
 async fn main() {
     let database = Database::new();
-    let config = Config::builder().add_file("Config.toml").build().unwrap();
-    let cors = CorsLayer::from(config.expect::<CorsConfig>());
 
-    let app = Application::from_config(config)
+    let app = Application::builder()
         .with_module::<ChatModule>()
         .with_provider(database)
-        .with_layer(cors)
         .build();
 
     app.run().await;
